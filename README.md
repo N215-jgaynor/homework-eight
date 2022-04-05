6ffb0a6a5c5e076206c8203e086a3aaa5175eba8
johngaynor <86327580+johngaynor@users.noreply.github.com>
2022-04-04 23:50:06 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# Homework Eight
+
+## Web 4 Link
+
+https://in-info-web4.informatics.iupui.edu/~jgaynor/homework-eight/#
+
+## Summary
+
+In addition to the skills developed in prior homework assignments (such as the use of responsive/adaptive design and variables), this task involved the use of mixins. I found these to be super helpful for replicating styling in different pages while still being able to make small changes as necessary. An example of a mixin in displayed below.
+
+```css
+@mixin promo($bgImage) {
+  height: 304px;
+  width: 235px;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)),
+    url($bgImage);
+}
+
+.tour-desc-header {
+  @include tourDescHeader("../images/tour-09.jpg");
+}
+
+.tour-desc-img-container {
+  @include tourDescImgContainer;
+}
+```
+
+This project simply continued the last one by finishing out the website with the special offers, blog and contact pages. In these pages I utilized both variables and mixins to optimize my code.
---
+++ app/app.js
@@ -0,0 +1,234 @@
+var homeContent = ` <div class="hero"></div>
+<div class="content">
+  <h1>HOME</h1>
+  <div class="products">
+    <div class="product">
+      <div class="prod-image"></div>
+      <div class="prod-desc">
+        <h4>Big City</h4>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+          dolores animi molestias explicabo? Labore commodi ex
+          asperiores impedit, fugiat dignissimos hic ut suscipit
+          doloremque, deleniti aspernatur cumque ipsa excepturi autem
+          officiis, eligendi minus fugit perspiciatis id laudantium ad
+          quo totam optio? Necessitatibus nesciunt debitis saepe
+          deserunt quis dicta impedit laborum.
+        </p>
+      </div>
+    </div>
+    <div class="product">
+      <div class="prod-image"></div>
+      <div class="prod-desc">
+        <h4>Big City</h4>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+          dolores animi molestias explicabo? Labore commodi ex
+          asperiores impedit, fugiat dignissimos hic ut suscipit
+          doloremque, deleniti aspernatur cumque ipsa excepturi autem
+          officiis, eligendi minus fugit perspiciatis id laudantium ad
+          quo totam optio? Necessitatibus nesciunt debitis saepe
+          deserunt quis dicta impedit laborum.
+        </p>
+      </div>
+    </div>
+    <div class="product">
+      <div class="prod-image"></div>
+      <div class="prod-desc">
+        <h4>Big City</h4>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+          dolores animi molestias explicabo? Labore commodi ex
+          asperiores impedit, fugiat dignissimos hic ut suscipit
+          doloremque, deleniti aspernatur cumque ipsa excepturi autem
+          officiis, eligendi minus fugit perspiciatis id laudantium ad
+          quo totam optio? Necessitatibus nesciunt debitis saepe
+          deserunt quis dicta impedit laborum.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>`;
+
+var aboutContent = ` <div class="hero"></div>
+<div class="content">
+  <h1>ABOUT</h1>
+  <div class="products">
+    <div class="product">
+      <div class="prod-image"></div>
+      <div class="prod-desc">
+        <h4>Big City</h4>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+          dolores animi molestias explicabo? Labore commodi ex
+          asperiores impedit, fugiat dignissimos hic ut suscipit
+          doloremque, deleniti aspernatur cumque ipsa excepturi autem
+          officiis, eligendi minus fugit perspiciatis id laudantium ad
+          quo totam optio? Necessitatibus nesciunt debitis saepe
+          deserunt quis dicta impedit laborum.
+        </p>
+      </div>
+    </div>
+    <div class="product">
+      <div class="prod-image"></div>
+      <div class="prod-desc">
+        <h4>Big City</h4>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+          dolores animi molestias explicabo? Labore commodi ex
+          asperiores impedit, fugiat dignissimos hic ut suscipit
+          doloremque, deleniti aspernatur cumque ipsa excepturi autem
+          officiis, eligendi minus fugit perspiciatis id laudantium ad
+          quo totam optio? Necessitatibus nesciunt debitis saepe
+          deserunt quis dicta impedit laborum.
+        </p>
+      </div>
+    </div>
+    <div class="product">
+      <div class="prod-image"></div>
+      <div class="prod-desc">
+        <h4>Big City</h4>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+          dolores animi molestias explicabo? Labore commodi ex
+          asperiores impedit, fugiat dignissimos hic ut suscipit
+          doloremque, deleniti aspernatur cumque ipsa excepturi autem
+          officiis, eligendi minus fugit perspiciatis id laudantium ad
+          quo totam optio? Necessitatibus nesciunt debitis saepe
+          deserunt quis dicta impedit laborum.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>`;
+
+var productsContent = ` <div class="hero"></div>
+<div class="content">
+  <h1>PRODUCTS</h1>
+  <div class="products">
+    <div class="product">
+      <div class="prod-image"></div>
+      <div class="prod-desc">
+        <h4>Big City</h4>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+          dolores animi molestias explicabo? Labore commodi ex
+          asperiores impedit, fugiat dignissimos hic ut suscipit
+          doloremque, deleniti aspernatur cumque ipsa excepturi autem
+          officiis, eligendi minus fugit perspiciatis id laudantium ad
+          quo totam optio? Necessitatibus nesciunt debitis saepe
+          deserunt quis dicta impedit laborum.
+        </p>
+      </div>
+    </div>
+    <div class="product">
+      <div class="prod-image"></div>
+      <div class="prod-desc">
+        <h4>Big City</h4>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+          dolores animi molestias explicabo? Labore commodi ex
+          asperiores impedit, fugiat dignissimos hic ut suscipit
+          doloremque, deleniti aspernatur cumque ipsa excepturi autem
+          officiis, eligendi minus fugit perspiciatis id laudantium ad
+          quo totam optio? Necessitatibus nesciunt debitis saepe
+          deserunt quis dicta impedit laborum.
+        </p>
+      </div>
+    </div>
+    <div class="product">
+      <div class="prod-image"></div>
+      <div class="prod-desc">
+        <h4>Big City</h4>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+          dolores animi molestias explicabo? Labore commodi ex
+          asperiores impedit, fugiat dignissimos hic ut suscipit
+          doloremque, deleniti aspernatur cumque ipsa excepturi autem
+          officiis, eligendi minus fugit perspiciatis id laudantium ad
+          quo totam optio? Necessitatibus nesciunt debitis saepe
+          deserunt quis dicta impedit laborum.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>`;
+
+var contactContent = ` <div class="hero"></div>
+<div class="content">
+  <h1>CONTACT</h1>
+  <div class="products">
+    <div class="product">
+      <div class="prod-image"></div>
+      <div class="prod-desc">
+        <h4>Big City</h4>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+          dolores animi molestias explicabo? Labore commodi ex
+          asperiores impedit, fugiat dignissimos hic ut suscipit
+          doloremque, deleniti aspernatur cumque ipsa excepturi autem
+          officiis, eligendi minus fugit perspiciatis id laudantium ad
+          quo totam optio? Necessitatibus nesciunt debitis saepe
+          deserunt quis dicta impedit laborum.
+        </p>
+      </div>
+    </div>
+    <div class="product">
+      <div class="prod-image"></div>
+      <div class="prod-desc">
+        <h4>Big City</h4>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+          dolores animi molestias explicabo? Labore commodi ex
+          asperiores impedit, fugiat dignissimos hic ut suscipit
+          doloremque, deleniti aspernatur cumque ipsa excepturi autem
+          officiis, eligendi minus fugit perspiciatis id laudantium ad
+          quo totam optio? Necessitatibus nesciunt debitis saepe
+          deserunt quis dicta impedit laborum.
+        </p>
+      </div>
+    </div>
+    <div class="product">
+      <div class="prod-image"></div>
+      <div class="prod-desc">
+        <h4>Big City</h4>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius
+          dolores animi molestias explicabo? Labore commodi ex
+          asperiores impedit, fugiat dignissimos hic ut suscipit
+          doloremque, deleniti aspernatur cumque ipsa excepturi autem
+          officiis, eligendi minus fugit perspiciatis id laudantium ad
+          quo totam optio? Necessitatibus nesciunt debitis saepe
+          deserunt quis dicta impedit laborum.
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+</div>`;
+
+function initListeners() {
+  $("nav a").click(function (e) {
+    let btnID = e.currentTarget.id;
+    loadPageContent(btnID);
+    console.log("click " + contentID);
+  });
+}
+
+function loadPageContent(pageName) {
+  let contentID = pageName + "Content";
+  $("#app").html(eval(contentID));
+}
+
+function initApp() {
+  initListeners();
+}
+
+$(document).ready(function () {
+  loadPageContent("home");
+  //   comment the loadPageContent out to keep the content on the page and style it before pushing it over to the app.js file. you can cheat the loading process by changing the loadPageContent to "about" will load that automatically
+  initApp();
+});
